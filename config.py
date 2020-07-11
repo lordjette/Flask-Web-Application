38acd2dc40f0523b75573aaaf0ec29252f93b926
Jett Lecaros <lordjette.leigh@gmail.com>
2020-07-12 00:34:35 +0800

    Chapter 2 changes SQLAlchemy Models

--- config.py
+++ config.py
@@ -1,12 +1,16 @@
 
 class Config(object):
-    pass
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class ProdConfig(Config):
     pass
 
+
 class DevConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    SQLALCHEMY_ECHO = True
 
 
 # :fire::fire::fire:
